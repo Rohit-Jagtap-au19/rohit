4e4fc3b8d4b6454cebcf67b26d3a2902f712fa11
Rohit <prjagtap90@gmail.com>
2022-01-19 23:11:13 +0530

    update

--- assignment/src/components/homepage/homepage.js
+++ assignment/src/components/homepage/homepage.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import "./homepage.css"
+import Postdata from '../data/Sampledata.json'
 
-const Homeage = ({setLoginUser}) =>{
-    return(
+const Homeage = ({ setLoginUser }) => {
+    return (
         <div className="homepage">
             <h1>HomePage</h1>
             <div className="button" onClick={() => setLoginUser({})}>Logout</div>
+                {Postdata.map((postdetails, index) => {
+                    return <div className="data">
+                        <h1>{postdetails.id +" "+ postdetails.title}</h1>
+                        <p>{postdetails.body}</p>
+                        </div>
+                })}
         </div>
     )
 }
